9a136ab763eea9a2a6d13d3142291049c5050446
T2hhbmEK <T2hhbmEK@gmail.com>
2025-05-22 10:34:44 +0800

    Fix xargs usage in clean.sh to remove ELF binaries and .exe files

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Find and delete ELF binaries
-find $CWD -type f ! -name "*.*" -exec file {} + | grep 'ELF' | cut -d: -f1 | tee /dev/stderr | xargs -0 rm -f
-find $CWD -type f -name "*.out" -exec file {} + | grep 'ELF' | cut -d: -f1 | tee /dev/stderr | xargs -0 rm -f
+find $CWD -type f ! -name "*.*" -exec file {} + | grep 'ELF' | cut -d: -f1 | tee /dev/stderr | xargs rm -f
+find $CWD -type f -name "*.out" -exec file {} + | grep 'ELF' | cut -d: -f1 | tee /dev/stderr | xargs rm -f
 
 # Find and delete .exe files
-find $CWD -type f -name "*.exe" -print0 | tee /dev/stderr | xargs -0 rm -f
+find $CWD -type f -name "*.exe" -print0 | tee /dev/stderr | xargs rm -f
