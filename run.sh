79fa780c84bc96c6ee9c9a973d501876654afea1
T2hhbmEK <T2hhbmEK@gmail.com>
2025-06-09 19:54:15 +0800

    Refactor run.sh to use a flags variable for compiler options and specify C++14 standard

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 src=$1
 bin=${1%.*}
-g++ $src -o $bin -static -Wall -O2 && $(realpath $bin)
+flags="\
+-Wall \
+-Wextra \
+-Wpedantic \
+-Wcast-align \
+-Wcast-qual \
+-Wdisabled-optimization \
+-Wformat=2 \
+-Wlogical-op \
+-Wmissing-include-dirs \
+-Woverloaded-virtual \
+-Wpointer-arith \
+-Wshadow \
+-Wswitch-enum \
+-Wvla \
+-fno-ms-extensions \
+"
+g++ $src -o $bin -std=c++14 -O2 -static $flags && $(realpath $bin)
