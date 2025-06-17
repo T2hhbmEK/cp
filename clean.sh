d71b6a94fe31e83298da9ba074c48636e48d66bf
T2hhbmEK <T2hhbmEK@gmail.com>
2025-06-17 12:20:28 +0800

    Refactor clean.sh to exclude text files from ELF binary deletion

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Find and delete ELF binaries
-find "$PWD" -type f -exec file {} + | grep -E 'ELF|executable' | cut -d: -f1 | tee /dev/stderr | xargs -r rm -f
+find "$PWD" -type f -exec file {} + | grep -E 'ELF|executable' | grep -v 'text' | cut -d: -f1 | tee /dev/stderr | xargs -r rm -f
 
 # Find and delete .exe files
 find "$PWD" -type f -name "*.exe" | tee /dev/stderr | xargs -r rm -f
