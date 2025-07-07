96f095c9eadaecf378ce3a1b69377a5f83f075dd
T2hhbmEK <T2hhbmEK@gmail.com>
2025-07-08 00:20:27 +0800

    Update settings.

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ if [[ "$(uname)" == "Darwin" ]]; then
     cxx=g++-15
     bin=${bin}.bin
 fi
-flags="-std=c++20 -Wall -Wextra -Wpedantic -Werror"
+# flags="-std=c++20 -Wall -Wextra -Wpedantic -Werror -Wno-unused-variable"
+flags="-std=c++20 -Wall -Wextra -Werror -Wno-unused-variable -Wno-keyword-macro"
 echo "$bin $@" 1>&2
 $cxx $src -o $bin $flags && $(realpath $bin) $@
 echo Press any key to continue... 1>&2
