3e97a77862313a740226c2d523e1d105a9268f6e
T2hhbmEK <T2hhbmEK@gmail.com>
2025-07-14 07:08:54 +0800

    Update P3373.cc

--- P3373.cc
+++ P3373.cc
@@ -2,93 +2,79 @@
 #define int long long
 using namespace std;
 
+template <typename T1 = int, typename T2 = int>
 struct SegTree {
-  struct node {
-    int sum = 0, add = 0, mul = 1;
-    explicit node() = default;
-    explicit node(int x) : sum(x) {}
-    explicit node(int x, int y, int z) : sum(x), add(y), mul(z) {}
-  };
-  int tl_, tr_, n4;
-  vector<node> t;
-  static int lch(int v) { return 2 * v; }
-  static int rch(int v) { return 2 * v + 1; }
-  static int mid(int tl, int tr) { return tl + (tr - tl) / 2; }
-  virtual node pushup(const node& tl, const node& tr) {
-    return node(tl.sum + tr.sum);
-  }
-  virtual void pushdown(node& v, int tl, int tr, const node& x) {
-    // y=kx+b y'=k'y+b'=k'kx+k'b+b'
-    v.sum = v.sum * x.mul + x.add * (tr - tl + 1);
-    v.mul = v.mul * x.mul;
-    v.add = v.add * x.mul + x.add;
-  }
-  void pushdown(int v, int tl, int tr) {
-    if (tl == tr) return;
-    pushdown(t[lch(v)], tl, mid(tl, tr), t[v]);
-    pushdown(t[rch(v)], mid(tl, tr) + 1, tr, t[v]);
-    pushup(v);
-  }
-  void pushup(int v) { t[v] = pushup(t[lch(v)], t[rch(v)]); }
-  void build(int v, int tl, int tr, int a[]) {
-    if (tl == tr) return void(t[v] = node(a[tl]));
-    build(lch(v), tl, mid(tl, tr), a);
-    build(rch(v), mid(tl, tr) + 1, tr, a);
-    pushup(v);
-  }
-  node query(int v, int tl, int tr, int l, int r) {
-    if (r < tl || tr < l) return node();
-    if (l <= tl && tr <= r) return t[v];
-    pushdown(v, tl, tr);
-    return pushup(query(lch(v), tl, mid(tl, tr), l, r),
-                  query(rch(v), mid(tl, tr) + 1, tr, l, r));
-  }
-  void update(int v, int tl, int tr, int l, int r, const node& x) {
-    if (r < tl || tr < l) return;
-    if (l <= tl && tr <= r) return pushdown(t[v], tl, tr, x);
-    pushdown(v, tl, tr);
-    update(lch(v), tl, mid(tl, tr), l, r, x);
-    update(rch(v), mid(tl, tr) + 1, tr, l, r, x);
-    pushup(v);
-  }
-  SegTree(int tl, int tr, int a[]) : tl_(tl), tr_(tr) {
-    int n = tr - tl + 1;
-    for (n4 = 1; n4 < n; n4 <<= 1);
-    t.resize(n4 <<= 1);
-    build(1, tl, tr, a);
-  }
-  int query(int l, int r) { return query(1, tl_, tr_, l, r).sum; }
-  void add(int l, int r, int x) { update(1, tl_, tr_, l, r, node(0, x, 1)); }
-  void mul(int l, int r, int x) { update(1, tl_, tr_, l, r, node(0, 0, x)); }
+  using node = pair<T1, T2>;
+  SegTree(int tl, int tr) : tl_(tl), tr_(tr), n4_((tr - tl + 1) * 4), t_(n4_) {}
+  void init(int a[]) { init(1, tl_, tr_, a); }
+  T1 qry(int l, int r) { return qry(1, tl_, tr_, l, r); }
+  void upd(int l, int r, T2 x) { upd(1, tl_, tr_, l, r, x); }
   friend ostream& operator<<(ostream& os, SegTree& t) {
-    for (int i = 1; i < t.n4; ++i) {
-      os << format("[{},{},{}] ", t.t[i].sum, t.t[i].add, t.t[i].mul)
-         << " \n"[i + 1 == 1 << static_cast<int>(log2(i + 1))];
-    }
-    for (int i = t.tl_; i <= t.tr_; ++i) os << t.query(i, i) << ' ';
+    for (int i = t.tl_; i <= t.tr_; ++i) os << t.qry(i, i) << ' ';
     return os;
+  }
+
+ private:
+  int tl_, tr_, n4_;
+  vector<pair<T1, T2>> t_;
+  virtual T1 up(const T1& tl, const T1& tr) = 0;
+  virtual void dn(node& v, int tl, int tr, const T2& x) = 0;
+  void dn(int v, int tl, int tr) {
+    if (tl == tr) return;
+    int tm = tl + (tr - tl) / 2;
+    dn(t_[v * 2], tl, tm, t_[v].second);
+    dn(t_[v * 2 + 1], tm + 1, tr, t_[v].second);
+    up(v);
+  }
+  void up(int v) { t_[v] = {up(t_[v * 2].first, t_[v * 2 + 1].first), T2{}}; }
+  void init(int v, int tl, int tr, int a[]) {
+    if (tl == tr) return void(t_[v].first = T1{a[tl]});
+    int tm = tl + (tr - tl) / 2;
+    init(v * 2, tl, tm, a), init(v * 2 + 1, tm + 1, tr, a), up(v);
+  }
+  T1 qry(int v, int tl, int tr, int l, int r) {
+    if (r < tl || tr < l) return T1{};
+    if (l <= tl && tr <= r) return t_[v].first;
+    dn(v, tl, tr);
+    int tm = tl + (tr - tl) / 2;
+    return up(qry(v * 2, tl, tm, l, r), qry(v * 2 + 1, tm + 1, tr, l, r));
+  }
+  void upd(int v, int tl, int tr, int l, int r, const T2& x) {
+    if (r < tl || tr < l) return;
+    if (l <= tl && tr <= r) return dn(t_[v], tl, tr, x);
+    dn(v, tl, tr);
+    int tm = tl + (tr - tl) / 2;
+    upd(v * 2, tl, tm, l, r, x), upd(v * 2 + 1, tm + 1, tr, l, r, x);
+    up(v);
   }
 };
 
-constexpr int MAXN = 100010;
-int MOD = numeric_limits<int>::max();
-struct SegTreeMod : SegTree {
-  SegTreeMod(int tl, int tr, int a[]) : SegTree(tl, tr, a) {}
-  node pushup(const node& tl, const node& tr) override {
-    return node((tl.sum + tr.sum) % MOD);
-  }
-  void pushdown(node& v, int tl, int tr, const node& x) override {
-    SegTree::pushdown(v, tl, tr, x);
-    v.sum %= MOD, v.add %= MOD, v.mul %= MOD;
-  }
+struct Lazy {
+  int k = 1, b = 0;
 };
-int n, q, a[MAXN];
+
+struct SegTreeAddMul : SegTree<int, Lazy> {
+  const int MOD;
+  SegTreeAddMul(int tl, int tr, int m) : SegTree<int, Lazy>(tl, tr), MOD(m) {}
+  int up(const int& tl, const int& tr) override { return (tl + tr) % MOD; }
+  void dn(node& v, int tl, int tr, const Lazy& x) override {
+    auto& [k, b] = v.second;
+    v.first = (v.first * x.k + x.b * (tr - tl + 1)) % MOD;
+    k = k * x.k % MOD, b = (b * x.k + x.b) % MOD;
+  }
+  void mul(int l, int r, int x) { upd(l, r, {.k = x, .b = 0}); }
+  void add(int l, int r, int x) { upd(l, r, {.k = 1, .b = x}); }
+};
+
+int n, q, m;
+
 signed main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
-  cin >> n >> q >> MOD;
+  cin >> n >> q >> m;
+  vector<int> a(1 + n);
   for (int i = 1; i <= n; ++i) cin >> a[i];
-  SegTreeMod t(1, n, a);
-  // cerr << t << endl;
+  SegTreeAddMul t(1, n, m);
+  t.init(a.data());
   while (q--) {
     int op, x, y, k;
     cin >> op;
@@ -100,9 +86,8 @@ signed main() {
       t.add(x, y, k);
     } else {
       cin >> x >> y;
-      cout << t.query(x, y) % MOD << '\n';
+      cout << t.qry(x, y) << '\n';
     }
-    // cerr << t << endl;
   }
   return 0;
 }
