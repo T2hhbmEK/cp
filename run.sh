c6a3bf6987a39a954a29aba1b9d413ef3d4ad05e
T2hhbmEK <T2hhbmEK@gmail.com>
2025-07-14 07:08:50 +0800

    Add debug settings

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ src=$1
 bin=${1%.*}
 shift 1
 if [[ "$(uname)" == "Darwin" ]]; then
-    # cxx=clang++
+    cxx=clang++
     cxx=g++-15
     bin=${bin}.bin
 fi
 # flags="-std=c++20 -Wall -Wextra -Wpedantic -Werror -Wno-unused-variable"
-flags="-std=c++20 -Wall -Wextra -Werror -Wno-unused-variable -Wno-keyword-macro -DDEBUG"
+flags="-std=c++20 -Wall -Wextra -DDEBUG"
 echo "$bin $@" 1>&2
 $cxx $src -g -o $bin $flags && $(realpath $bin) $@
 echo Press any key to continue... 1>&2
